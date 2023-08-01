02a23239f499041bc3eefc83ef15664fb6e2eb62
Your Name <benito1karkada@gmail.com>
2023-08-01 17:12:52 -0400

    yesss

--- client/src/components/tiles.js
+++ client/src/components/tiles.js
@@ -3,7 +3,7 @@ export default function Tiles() {
     return (
       <div>
         <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <InstagramEmbed url="https://www.instagram.com/p/CvaFncgOCw3/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==" width={328} />
+          <InstagramEmbed url="https://www.instagram.com/p/CvaQqo-uhko/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==" width={328} />
         </div>
       </div>
     )}
