2d23daa3e4266bd42b6df2ed23786e9ad3c5766a
Your Name <benito1karkada@gmail.com>
2023-08-01 15:34:13 -0400

    try this

--- client/src/components/tiles.js
+++ client/src/components/tiles.js
@@ -1,8 +1,7 @@
 export default function Tiles() {
     return (
       <div class="tiles">
-        <blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/CvaQqo-uhko/" data-instgrm-version="13">
-        </blockquote>
+        <img src = "https://scontent.cdninstagram.com/v/t51.2885-15/365007454_247360694796668_2299864135880267412_n.jpg?stp=dst-jpg_e35&_nc_ht=scontent.cdninstagram.com&_nc_cat=108&_nc_ohc=oJOV1KDRSWcAX9koh2J&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzE1OTkxMTM4NzUxODA4MTMyMA%3D%3D.2-ccb7-5&oh=00_AfC7r3jADvmYWlZknB4sTfoJCG_DOmEh4mPGXr2P5xoL1Q&oe=64CE8029&_nc_sid=10d13b" />
       </div>
     );
   }
