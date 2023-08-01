01a8a5545b990008f15c38af5447e883f1989ef4
notSpainChris <christopher.e.lenhardt@gmail.com>
2023-08-01 16:47:00 -0400

    App and style sheet

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header';
+import Tiles from './components/tiles';
 
 function App() {
   return (
     <body>
       <Header />
+      <Tiles />
     </body>
   );
 }
