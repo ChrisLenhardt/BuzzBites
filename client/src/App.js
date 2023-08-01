c8056809ab1d9386dfca0bca8f86e833d45a7699
Your Name <benito1karkada@gmail.com>
2023-08-01 13:20:24 -0400

    yes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Post from './components/post';
 import Header from './components/header';
 
 function App() {
   return (
     <body>
       <Header />
-      <Post />
     </body>
   );
 }
