25b397208d5507c66527065065e615799c511aea
Your Name <benito1karkada@gmail.com>
2023-08-01 18:06:23 -0400

    new pics

--- client/src/components/tiles.js
+++ client/src/components/tiles.js
@@ -1,10 +1,14 @@
 import { InstagramEmbed } from 'react-social-media-embed';
-import picture from "./images/m4.jpeg"
+import picture from "./images/pasta.jpg"
+import picture2 from "./images/chick.jpg"
+import picture3 from "./images/sss.jpg"
 export default function Tiles() {
     return (
       <div>
         <div style={{ display: 'flex', justifyContent: 'center' }}>
           <img src={picture} alt=''></img>
+          <img src={picture2} alt=''></img>
+          <img src={picture3} alt=''></img>
         </div>
       </div>
     )}
