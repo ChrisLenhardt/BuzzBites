2c4cc35d988b018d302db12fb8248854e968418f
Your Name <benito1karkada@gmail.com>
2023-08-03 12:22:11 -0400

    updates to cart page

--- client/src/components/shoppingCart.js
+++ client/src/components/shoppingCart.js
@@ -5,9 +5,14 @@ export default function ShoppingCart () {
     return(
         <div>
         <Header />
-      <h1>
-        Your Shopping Cart is Empty
-      </h1>
-      </div>
+        <h1>Shopping Cart</h1>
+        <ul id="cart-list">
+        </ul>
+        <div id="subtotal">
+          <p>Subtotal: <span id="subtotal-amount">$0.00</span></p>
+          <p>Tax: <span id="tax-amount">$0.00</span></p>
+          <p>Total: <span id="total-amount">$0.00</span></p>
+        </div>
+        </div>
     )
 }
