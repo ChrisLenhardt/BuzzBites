6a2927db8a11501a87473d19a7f507941486fc35
Your Name <benito1karkada@gmail.com>
2023-08-06 16:46:10 -0400

    I DID IT

--- server/server.js
+++ server/server.js
@@ -1,73 +1,47 @@
 const express = require('express');
+const { ObjectId, Double } = require('mongodb');
 const mongoose = require('mongoose');
-const { MongoClient } = require("mongodb");
+const Decimal128 = require('mongoose/lib/schema/decimal128');
 const app = express();
+const port = 8000;
 
-const port = 8000
-const uri = 'mongodb+srv://chrisL:Cl02082005@cluster0.nzksy4h.mongodb.net/'
-const client = new MongoClient(uri);
+const uri = 'mongodb+srv://chrisL:Cl02082005@cluster0.nzksy4h.mongodb.net/buzzbitess';
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`App is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // Exit the application on connection error
+  });
 
-async function connect(){
-  try{
-    await mongoose.connect(uri);
-    console.log("connected to mongodb")
+// Mongoose schema and model (modify according to your data structure)
+const menuSchema = new mongoose.Schema({
+  _id: ObjectId,
+  Cuisine: String,
+  Ingredients: String,
+  Name: String,
+  Price: Decimal128,
+}, {collection: 'menus' });
+
+const Menu = mongoose.model('Menu', menuSchema);
+
+// Route to get menu data from MongoDB
+app.get('/api/menu', async (req, res) => {
+  console.log('Request received for /api/menu'); // Debug statement
+
+  try {
+    // Fetch menu data from MongoDB
+    const menuItems = await Menu.find().lean();
+
+    console.log('Menu Items:', menuItems); // Debug statement
+
+    res.json(menuItems);
+  } catch (error) {
+    console.error('Error retrieving menu data:', error);
+    res.status(500).json({ error: 'An error occurred while retrieving menu data.' });
   }
-  catch(error){
-    console.error(error);
-  }
-}
-
-connect();
-
-app.listen(port, () => 
-  (console.log(`App is running on port ${port}`))
-);
-
-
-app.get('/apiTest', async (req,res) =>{
-  
-  try{
-    const db = client.db('sample_mflix')
-    const collection = db.collection('users')
-
-    const results = await collection.find();
-    res.json(results.next());
-    console.log(results.next());
-  }
-  catch(error){
-    console.log("uh oh");
-  }
-
-})
-
-app.get('/helloWorld', async (req,res) =>{
-  await client.connect();
-  res.json("Hello World");
-  
-})
-
-
-async function run(){
-    try{
-        await client.connect();
-
-        const db = client.db('sample_mflix')
-        const collection = db.collection('users')
-
-        const cursor = collection.find({name: "Jon Snow"});
-
-        cursor.toArray((err,results)=>{
-          if(err){
-            console.log(err);
-          }
-
-          console.log(results);
-        })
-            //for each is deprecated, i will replace it later
-    }
-    finally{
-        await client.close();
-    }
-}
-
-//run();
+});
