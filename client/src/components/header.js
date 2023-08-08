d3bc8fa0a7ab41a287ec0d17905d9f96186cb892
Your Name <benito1karkada@gmail.com>
2023-08-08 19:02:48 -0400

    major ui update

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,15 +1,18 @@
 import Cart from "./cart";
 
-export default function Header(){
-    return(
-            <div class="header">
-            <h1 class="headerTitle">
-                <a href="/" class="type1Link">BuzzBites</a>
-            </h1>
-            
-            <Cart />
-
- 
+export default function Header() {
+  return (
+    <div>
+      <div class="header">
+        <h1 class="headerTitle">buzz</h1>
+        <h1 class="headerTitle2">BITES</h1>
+        <div class="navBar">
+          <h1 class="navTitles">HOME</h1>
+          <h1 class="navTitles">MENU</h1>
         </div>
-    );
+
+        <Cart />
+      </div>
+    </div>  
+  );
 }
--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,17 +1,35 @@
+import React from "react"; // Import React to use JSX
+
 import Header from "./header";
 import Italian from "./italian";
 import Indian from "./indian";
 import Middle from "./middle";
 import Western from "./western";
+import picture from "./images/yellowting.png";
+import picture2 from "./images/homepasta.png";
 
-export default function Home () {
-    return (
-        <div id="root">
-        <Header></Header>
-        <h2 class="subTitle">
-            This Week's Menu
-        </h2> 
-        <Italian />
+export default function Home() {
+  return (
+    <div id="root">
+      <div className="temp">
+        <Header />
+      </div>
+      <div className="yellow">
+        <div className="text-boxes">
+          <div className="text-box">
+            <p className="orange-text">ARE YOU HUNGRY?</p>
+            <p className="white-text bigger-text">DON'T WAIT!</p>
+            <p className="white-text smaller-text">
+              this is just a test of the paragraph
+            </p>
+            <button className="orange-button">ORDER NOW</button>
+          </div>
+          <div className="image-container">
+            <img src={picture} alt="Picture 1" />
+            <img src={picture2} alt="Picture 2" className="overlay-image" />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
